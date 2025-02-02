9cc8948a5383b0d92cb6e0e1d100425879f5a122
Venkata Padmanabha Karteek Yeluru <91717475+Karteekyvp@users.noreply.github.com>
2025-02-01 20:43:02 -0500

    Update deliver.bat

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -1,8 +1,5 @@
-
 @echo off
 echo Installing the Maven-built Java application into the local Maven repository...
-
-REM Install the package using Maven
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 
 REM Extract the project name from pom.xml
@@ -12,4 +9,5 @@ REM Extract the project version from pom.xml
 for /f "delims=" %%a in ('mvn help:evaluate -Dexpression=project.version ^| findstr /v /c:"["') do set VERSION=%%a
 
 echo Running the Java application...
-java -jar target\%NAME%-%VERSION%.jar
+java -jar target\%NAME%-%VERSION%.jar >> output.log 2>&1
+type output.log
